fe10e7e5f58c8b393011f293944ebc34fc279ca4
KrishnaSriR <KrishnaSriR@users.noreply.github.com>
2023-11-09 23:44:02 -0600

    Lab 12 changes

--- controllers/education.js
+++ controllers/education.js
@@ -4,9 +4,15 @@ exports.education_list = function(req, res) {
 res.send('NOT IMPLEMENTED: Education list');
 };
 // for a specific Education.
-exports.education_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Education detail: ' + req.params.id);
-};
+exports.education_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Education.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+}};
 // Handle Education create on POST.
 exports.education_create_post = async function(req, res) {
     console.log(req.body)
@@ -32,9 +38,22 @@ exports.education_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Education delete DELETE ' + req.params.id);
 };
 // Handle Education update form on PUT.
-exports.education_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Education update PUT' + req.params.id);
-};
+exports.education_update_put = async function(req, res) {
+        console.log(`update on id ${req.params.id} with body
+        ${JSON.stringify(req.body)}`)
+        try {
+        let toUpdate = await Education.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.subject) toUpdate.subject = req.body.subject;
+        if(req.body.grade) toUpdate.grade = req.body.grade;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+        } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
+    }};
 
 // List of all Education objects
 exports.education_list = async function(req, res) {
