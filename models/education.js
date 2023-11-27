6ac84c6d5b84727c7aacd347423ae3ca36f983b7
KrishnaSriR <KrishnaSriR@users.noreply.github.com>
2023-11-27 12:26:53 -0600

    A13 Part 5,6 changes

--- models/education.js
+++ models/education.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const educationSchema = mongoose.Schema({
 subject: String,
 grade: String,
-marks: Number
+marks: {
+    type: Number,
+    min: 0,  // Minimum allowed value
+    max: 100,  // Maximum allowed value
+}
 })
 module.exports = mongoose.model("education",educationSchema)
 
